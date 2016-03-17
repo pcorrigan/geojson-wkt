f7b424c14280957af78114d1716bec070484dab1
Peter Corrigan <peter.corrigan@gmail.com>
2016-03-17 22:27:29 +0000

    Fix missing comma-bug at EOL

--- geoj-wkt.pl
+++ geoj-wkt.pl
@@ -1,4 +1,4 @@
-#!/Users/peter/perl5/perlbrew/perls/perl-5.18.2/bin/perl
+#!/usr/bin/env perl
 use warnings;
 use strict;
 
@@ -13,26 +13,33 @@ die "Usage: $0 URL-Townland-file.txt" unless @ARGV;
 
 my $PI = atan2( 1, 1 ) * 4;
 my $browser = LWP::UserAgent->new;
+path("wkt-output")->mkpath;
 
-while (<>) {
+while  (<>) {
     chomp;
+    
     #ignore comments and blank lines
-    next unless ( ( /\w+/ ) or ( /^\s*#/) );
+    next if (  /^\s*$/||/#/) ; 
+    
     my ( $url, $townland ) = split(/\|/);
     say "Using:", "$url for $townland";
     my $response = $browser->get( 'http://www.townlands.ie' . $url );
     die "Can't get $url -- ", $response->status_line
         unless $response->is_success;
     my $townland_html = $response->content;
+    
+    #Scrape
     my ($geojson) = $townland_html =~ /.*L.geoJson\((.*)\)\.addTo\(map\)/gsmx;
     $geojson =~ s/\'/"/g;    # Make double-quotes for strict parser
     say "Got GeoJSON for $townland";
+    
     my $path      = path("./wkt-output/$townland.wkt");
     my $json_text = decode_json $geojson;
     unfold( $json_text, $path );
 }
 
-sub unfold {
+
+sub  unfold {
     my ( $js, $path ) = @_;
     $path->spew_utf8( $js->{geometry}->{type}, "((\n" );
     for my $coord_set ( @{ $js->{geometry}->{coordinates} } ) {
@@ -43,7 +50,7 @@ sub unfold {
 
 #-9.8815003 53.2977326, -9.8812643 53.2981558, -9.8808029 53.298284, -9.8802772 53.298502, -9.8798803 53.2984892, -9.8796764 53.2982904, -9.8796335 53.2981173, -9.8796442 53.2979826
 
-    sub to_merc {
+    sub  to_merc {
         my ( $lat, $lon ) = @_;
 
         #say "lat is $lat and lon is $lon";
@@ -52,7 +59,7 @@ sub unfold {
         $y = $y * 20037508.34 / 180;
         $x = sprintf( "%.2f", ($x) );
         $y = sprintf( "%.2f", ($y) );
-        return "$x $y";
+        return "$x $y,";
     }
 
     # Callbacks for edit_utf8
